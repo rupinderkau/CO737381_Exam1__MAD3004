a5c74295645306ec5bee815a49e9bb0cb80bbab9
MacStudent <macstudent@20x-imacxx-m-386.local>
2018-07-23 13:18:55 -0400

    Initial Commit

---
+++ 737381_Exam1_MAD30004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  737381_Exam1_MAD30004
+//
+//  Created by MacStudent on 2018-07-23.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
